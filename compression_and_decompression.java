1fc5d57480c8cb5056ffb6d3e9178ab46a41792f
Rhea Roy <rroy.rhea@gmail.com>
2021-05-09 17:28:23 -0700

    removed unused imports

--- compression_and_decompression.java
+++ compression_and_decompression.java
@@ -7,8 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class compression_and_decompression {
