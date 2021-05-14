91cad237d95dace9ced925dd363af6eb330f2c66
Rhea Roy <rroy.rhea@gmail.com>
2021-05-14 06:19:07 -0700

    BitInputStream

--- huffman_project.java
+++ huffman_project.java
@@ -97,130 +97,65 @@ public class huffman_project {
 		String text2 = new String(b2);
 
 		BitInputStream BinIn = new BitInputStream("objOut2.txt");
-
+		System.out.println(BinIn.readBit());
+		BinIn.close();
 	}
 
 
 	public static class BitInputStream {
-		private static final int EOF = -1; // end of file
+	    private FileInputStream input;
+	    private int digits;     // next set of digits (buffer)
+	    private int numDigits;  // how many digits from buffer have been used
 
-		private BufferedInputStream in; // the input stream
-		private int buffer; // one character buffer
-		private int n; // number of bits left in buffer
+	    private static final int BYTE_SIZE = 8;  // digits per byte
 
-		public BitInputStream(InputStream is) {
-			in = new BufferedInputStream(is);
-			fillBuffer();
-		}
+	    // pre : given file name is legal
+	    // post: creates a BitInputStream reading input from the file
+	    public BitInputStream(String file) {
+	        try {
+	            input = new FileInputStream(file);
+	        } catch (IOException e) {
+	            throw new RuntimeException(e.toString());
+	        }
+	        nextByte();
+	    }
 
-		public BitInputStream(String fileName) {
+	    // post: reads next bit from input (-1 if at end of file)
+	    public int readBit() {
+	        // if at eof, return -1
+	        if (digits == -1)
+	            return -1;
+	        int result = digits % 2;
+	        digits /= 2;
+	        numDigits++;
+	        if (numDigits == BYTE_SIZE)
+	            nextByte();
+	        return result;
+	    }
 
-			try {
-				// first try to read file from local file system
-				File file = new File(fileName);
-				if (file.exists()) {
-					FileInputStream fis = new FileInputStream(file);
-					in = new BufferedInputStream(fis);
-					fillBuffer();
-				}
-			} catch (IOException ioe) {
-				System.err.println("Could not open " + fileName);
-			}
-		}
+	    // post: refreshes the internal buffer with the next BYTE_SIZE bits
+	    private void nextByte() {
+	        try {
+	            digits = input.read();
+	        } catch (IOException e) {
+	            throw new RuntimeException(e.toString());
+	        }
+	        numDigits = 0;
+	    }
 
-		public BitInputStream(File file) {
+	    // post: input is closed
+	    public void close() {
+	        try {
+	            input.close();
+	        } catch (IOException e) {
+	            throw new RuntimeException(e.toString());
+	        }
+	    }
 
-			try {
-				if (file.exists()) {
-					FileInputStream fis = new FileInputStream(file);
-					in = new BufferedInputStream(fis);
-					fillBuffer();
-				}
-			} catch (IOException ioe) {
-				System.err.println("Could not open " + file.getName());
-			}
-		}
-
-		private void fillBuffer() {
-			try {
-				buffer = in.read();
-				n = 8;
-			} catch (IOException e) {
-				System.err.println("EOF");
-				buffer = EOF;
-				n = -1;
-			}
-		}
-
-		public boolean exists() {
-			return in != null;
-		}
-
-		public boolean isEmpty() {
-			return buffer == EOF;
-		}
-
-		public boolean readBoolean() {
-			if (isEmpty())
-				throw new NoSuchElementException("Reading from empty input stream");
-			n--;
-			boolean bit = ((buffer >> n) & 1) == 1;
-			if (n == 0)
-				fillBuffer();
-			return bit;
-		}
-
-		public char readChar() {
-			if (isEmpty())
-				throw new NoSuchElementException("Reading from empty input stream");
-
-			// special case when aligned byte
-			if (n == 8) {
-				int x = buffer;
-				fillBuffer();
-				return (char) (x & 0xff);
-			}
-
-			// combine last N bits of current buffer with first 8-N bits of new buffer
-			int x = buffer;
-			x <<= (8 - n);
-			int oldN = n;
-			fillBuffer();
-			if (isEmpty())
-				throw new NoSuchElementException("Reading from empty input stream");
-			n = oldN;
-			x |= (buffer >>> n);
-			return (char) (x & 0xff);
-			// the above code doesn't quite work for the last character if N = 8
-			// because buffer will be -1
-		}
-
-		public String readString() {
-			if (isEmpty())
-				throw new NoSuchElementException("Reading from empty input stream");
-
-			StringBuilder sb = new StringBuilder();
-			while (!isEmpty()) {
-				char c = readChar();
-				sb.append(c);
-			}
-			return sb.toString();
-		}
-
-		public int readInt() {
-			int x = 0;
-			for (int i = 0; i < 4; i++) {
-				char c = readChar();
-				x <<= 8;
-				x |= c;
-			}
-			return x;
-		}
-
-		public byte readByte() {
-			char c = readChar();
-			return (byte) (c & 0xff);
-		}
+	    // included to ensure that the stream is closed
+	    protected void finalize() {
+	        close();
+	    }
 	}
 
 	static class BitOutputStream {
